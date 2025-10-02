562649f73cbe6ad87d79b8fb512a6827ad2920b3
Philip Damgaard Sørensen <60739799+FRagIC17@users.noreply.github.com>
2025-10-02 08:20:40 +0200

    Delete TytechDB delete script and drop tables updated

--- TytechDB - Delete script.sql
+++ TytechDB - Delete script.sql
@@ -1,33 +1,18 @@
--- Det der står under er et delete script der reseter id også.
+-- Det der stÃ¥r under er et delete script der reseter id ogsÃ¥.
 USE TytechDB
 GO
 
-DELETE FROM Catalog.Categories
 
-DBCC CHECKIDENT ('Catalog.Categories', RESEED, 0);
-
-DELETE FROM Catalog.Products
-
-DBCC CHECKIDENT ('Catalog.Products', RESEED, 0);
-
-DELETE FROM Catalog.Suppliers
-
-DBCC CHECKIDENT ('Catalog.Suppliers', RESEED, 0);
-
-DELETE FROM Catalog.SupplierProducts 
-
-DELETE FROM Sales.StatusEnnums
-
-DBCC CHECKIDENT ('Sales.StatusEnnums', RESEED, 0);
-
-ALTER TABLE Sales.StatusEnnums
-ALTER COLUMN order_status VARCHAR(13) NOT NULL;
-
-ALTER TABLE Sales.StatusEnnums
-ALTER COLUMN shipment_status VARCHAR(13) NOT NULL;
-
-ALTER TABLE Sales.StatusEnnums
-ALTER COLUMN returns_status VARCHAR(13) NOT NULL;
-
-ALTER TABLE Sales.StatusEnnums
-ALTER COLUMN payment_status VARCHAR(13) NOT NULL;
+DROP TABLE Promotions.Discounts;
+DROP TABLE Sales.ReturnOrders
+DROP TABLE Sales.Shipments
+DROP TABLE Sales.Payments
+DROP TABLE Sales.Order_lines
+DROP TABLE Sales.Orders
+DROP TABLE Sales.StatusEnnums
+DROP TABLE Customers.Customer
+DROP TABLE Catalog.Inventory
+DROP TABLE Catalog.SupplierProducts
+DROP TABLE Catalog.Products
+DROP TABLE Catalog.Suppliers
+DROP TABLE Catalog.Categories
